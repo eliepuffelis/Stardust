13a06be9b539825084632164cf10e67f67827805
Daniel Tatarkin <danieltatarkin@gmail.com>
2018-09-23 18:36:03 -0700

    ---

--- firebase-login.js
+++ firebase-login.js
@@ -1,1 +1,11 @@
 var firebase = require("firebase");
+
+var config = {
+    apiKey: "AIzaSyAGyI0h7geBuNuRrUPB8yVm9QIo7de3uBs",
+    authDomain: "stardust-4808d.firebaseapp.com",
+    databaseURL: "https://stardust-4808d.firebaseio.com",
+    projectId: "stardust-4808d",
+    storageBucket: "stardust-4808d.appspot.com",
+    messagingSenderId: "339517051071"
+};
+firebase.initializeApp(config);
